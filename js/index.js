9dafb93a29c9ada3f6c718b995751b7f25fd95f7
yxq <769421530@qq.com>
2017-04-13 16:54:15 +0800

    我的网页

---
+++ js/index.js
@@ -0,0 +1,11 @@
+/**
+ * Created by yxq on 2017/3/17.
+ */
+window.onload = function () {
+
+    $(".vita-l").onmouseover = function () {
+        $(".info1").addClass("animated swing");
+    }
+
+
+}
